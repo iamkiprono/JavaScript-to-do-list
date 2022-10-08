8abdffbf64039bc1a2e6a02d3c7bd15ff4d9316e
iamkiprono <101986591+iamkiprono@users.noreply.github.com>
2022-10-08 10:14:16 +0300

    app.js, index.html, style.css

---
+++ app.js
@@ -0,0 +1,7 @@
+// Selectors
+const todoInput = document.queryselector(".todo-input");
+const todoButton = document.queryselector(".todo-button");
+const todoList = document.queryselector(".todo-list");
+
+// Event Listeners
+todoButton.addEventlistener("click", addTodo);
